414a2c61336cd57bf5c6b3c9d14db52cb1e8ff9f
GuiDeto <detonix@gmail.com>
2020-02-06 15:14:33 -0200

    mysql add

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const app = require('express')();
 const path = require('path');
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'chatdb'
+});
+con.connect(function (err) {
+    if (err) throw err;
+});
 
 // const app = express();
 const server = require('http').createServer(app);
@@ -14,23 +24,49 @@ app.use('/', (req, res) => {
     res.render('index.html');
 });
 
-let messages = [];
-
 // server side code
+let messages = [];
 io.sockets.on('connection', function (socket) {
     socket.on('create', function (room) {
+
         socket.join(room, function () {
+
             console.log(`Id: ${socket.id} Sala: ${room}`);
-            if(messages.room == room)io.sockets.to(socket.id).emit('previousMessage', messages);
+            showOldMessagesChat(room, socket.id);
+            
+
         });
 
-        socket.on('sendMessage', data => {
-            messages.push(data);
-            socket.broadcast.to(room).emit('sendMessage', data);
-        });
+    });
+
+    socket.on('sendMessage', data => {
+        var user = data.user,
+            room = data.room,
+            message = data.message;
+        insertMessageDB(room, user, message);
+
+        socket.broadcast.in(data.room).emit('sendMessage', data);
     });
 });
 
 server.listen(3000, data => {
     console.log('Server on 3000');
 });
+
+function showOldMessagesChat(r, sckId) {
+    let dados = [];
+    con.query("SELECT * FROM chat WHERE room='" + r + "'", function (err, result) {
+        if (err) throw err;
+        for (message of result) {
+            dados.push( {"user":message.user, "date":message.date_add_message,"message":message.message} );
+        }
+        io.sockets.in(sckId).emit('previousMessage', dados);
+    });
+}
+
+function insertMessageDB(r, u, m) {
+    con.query('INSERT INTO chat (room, user, message) VALUES (\'' + r + '\',\'' + u + '\',\'' + m + '\')', function (error, results, fields) {
+        if (error) throw error;
+        if (results.affectedRows) console.log('Cadastro inserido no banco!');
+    });
+}
